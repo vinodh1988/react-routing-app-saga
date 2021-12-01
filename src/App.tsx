13d3ee9c6096214ce316a0e8f2273685c56e292f
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-01 14:59:48 +0530

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const Home=()=>{
+  return (
+    <div id="home">
+<nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div className="container-fluid">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link active" href="#">ABC APP</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About us</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Quotes For the Day</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+  )
+}
+export default Home;
